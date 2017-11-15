9b605e9b099535ec51f939a46d651ccb1bcd2033
jjwumaster <jjwumaster@gmail.com>
2017-11-15 10:22:22 -0500

    built out relationship between bikes and trips

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,6 @@
 class Bike < ActiveRecord::Base
+  has_many :trips, foreign_key: 'city_bike_id', primary_key: 'city_bike_id'
+
+
+  has_many :stations, through: :trips # do we need to add start/end stations
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,4 @@
 class Station < ActiveRecord::Base
+  has_many :trips
+  has_many :bikes, through: :trips
 end
