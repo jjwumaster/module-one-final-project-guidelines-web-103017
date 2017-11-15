977a9454bf2d184e3bff0217f848686aaf5de9cc
jjwumaster <jjwumaster@gmail.com>
2017-11-15 10:50:04 -0500

    all connections between models working except trip >> station

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,5 @@
 class Bike < ActiveRecord::Base
   has_many :trips, foreign_key: 'city_bike_id', primary_key: 'city_bike_id'
-
-
-  has_many :stations, through: :trips # do we need to add start/end stations
+  has_many :start_stations, through: :trips # do we need to add start/end stations
+  has_many :end_stations, through: :trips
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,7 @@
 class Station < ActiveRecord::Base
-  has_many :trips
+  # has_many :trips, foreign_key: 'start_station_id', primary_key: 'city_station_id'
+  # has_many :trips, foreign_key: 'end_station_id', primary_key: 'city_station_id'
+
+  has_many :trips, foreign_key: 'start_station_id', primary_key: 'city_station_id'
   has_many :bikes, through: :trips
 end
